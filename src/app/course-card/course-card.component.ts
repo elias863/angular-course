f095290f8d34f5dde7a6fb4894d850d605d30865
Elias86 <eliasmurgia86@gmail.com>
2024-11-11 15:40:59 +0100

    multiple css classes with [ngClass] directive

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
 import { Component,EventEmitter,input,Input,OnInit, Output } from '@angular/core';
 import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'course-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
@@ -29,5 +30,12 @@ export class CourseCardComponent implements OnInit{
 
   onCourseViewed(){
     this.courseEmitter.emit(this.course);
+  }
+
+  cardClasses(){
+    return{
+        'beginner':this.course.category == 'BEGINNER',
+        'course-card': true
+    }
   }
 }
